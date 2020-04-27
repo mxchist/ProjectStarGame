6e14f9a7ae16f1ad9356257837d715af361b758f
Alexey Kutepov <alkutepov@gmail.com>
2020-04-27 20:07:28 +0300

    Lesson 2 Part 1

--- core/src/ru/geekbrains/StarGame.java
+++ core/src/ru/geekbrains/StarGame.java
@@ -5,37 +5,49 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.math.Vector2;
 
 public class StarGame extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture img;
-	TextureRegion region;
-
-	float x, y;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
 		img = new Texture("badlogic.jpg");
-		region = new TextureRegion(img, 50, 50, 100, 100);
-		x = 0;
-		y = 0;
+
+		Vector2 v1 = new Vector2(2, 2);
+		Vector2 v2 = new Vector2(2, 2);
+		v1.add(v2);
+		System.out.println("v1 add v2 v1.x = " + v1.x + " v1.y = " + v1.y);
+		Vector2 v3 = v1.cpy().add(v2);
+		System.out.println("v1 add v2 v1.x = " + v1.x + " v1.y = " + v1.y);
+		System.out.println("v1 add v2 v3.x = " + v3.x + " v3.y = " + v3.y);
+
+		v1.set(7, 2);
+		v2.set(3, 3);
+		v1.sub(v2);
+		System.out.println("v1 sub v2 v1.x = " + v1.x + " v1.y = " + v1.y);
+		System.out.println("v1.len() = " + v1.len());
+
+		v1.nor();
+		System.out.println("v1.len() = " + v1.len());
+		System.out.println("v1.nor(); v1.x = " + v1.x + " v1.y = " + v1.y);
+
+		v1.set(2, 5);
+		v2.set(4, 2);
+		v1.nor();
+		v2.nor();
+		System.out.println(Math.acos(v1.dot(v2)));
+
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(0.3f, 0.2f, 0.5f, 1);
+		Gdx.gl.glClearColor(0.3f, 0.2f, 0.1f, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		x += 1f;
-		y += 1f;
 		batch.begin();
-		batch.setColor(0.5f, 0.5f, 0.5f, 1f);
-		batch.draw(img, x, y);
-		batch.setColor(0.1f, 0.7f, 0.1f, 0.5f);
-		batch.draw(region, 100, 100, 200, 200);
-		batch.setColor(0.6f, 0.1f, 0.4f, 0.5f);
-		batch.draw(region, 300, 300, 200, 200);
+		batch.draw(img, 0, 0);
 		batch.end();
 	}
 	
